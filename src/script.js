ce85f97eb348fef8de39a4ad72d80b8a9f0b99f5
“Brenno <brennoeudes@gmail.com>
2024-02-18 21:35:42 -0300

    referral-form, link generator function concluded

--- src/script.js
+++ src/script.js
@@ -12,11 +12,48 @@ navbar.addEventListener("click", function () {
   if (isBurguerVisible) {
     burguerMenu.style.display = "none";
     closeBtn.style.display = "block";
-
   } else {
     burguerMenu.style.display = "block";
     closeBtn.style.display = "none";
   }
 
   isBurguerVisible = !isBurguerVisible;
+});
+
+document
+  .getElementById("email-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailInput = document.getElementById("user-email");
+    const email = emailInput.value.trim();
+
+    const correctEmail = document.getElementById("correct-email");
+    const invalidEmail = document.getElementById("invalid-email");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "" || !emailRegex.test(email)) {
+      invalidEmail.style.display = "block";
+      correctEmail.style.display = "none";
+    } else {
+      invalidEmail.style.display = "none";
+      correctEmail.style.display = "flex";
+
+      document.getElementById("email-form").style.display = "none";
+      document.getElementById("referral-form").style.display = "flex";
+
+      const encodedEmail = btoa(email);
+      const referralLinkInput = document.getElementById("referral-link");
+      referralLinkInput.value = `https://ratepunk.com/referral?email=${encodedEmail}`;
+    }
+  });
+
+document.getElementById("copy-btn").addEventListener("click", function () {
+  const referralLinkInput = document.getElementById("referral-link");
+  referralLinkInput.select();
+  document.execCommand("copy");
+
+  const emailForm = document.getElementById("email-form");
+  emailForm.reset();
 });
